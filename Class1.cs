746a6e1df99e5149f01d80213c5a8d7746f0e08b
thanhminh92it <thanhminh92it@gmail.com>
2014-10-16 16:09:34 +0700

    CTest new file
    chó Lã MInh

--- Class1.cs
+++ Class1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace Test
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("aaa");
-        }
     }
 }
